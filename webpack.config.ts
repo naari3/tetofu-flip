c2c6e228d8433130b7f836630112f1906e48866a
naari3 <naari.named@gmail.com>
2021-02-05 22:39:07 +0900

    Implement initializers
    https://qiita.com/markey/items/ea9ed18a1a243b39e06e

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import { Configuration } from "webpack";
+import path from "path";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const config: Configuration = {
+  entry: {
+    content_scripts: path.join(__dirname, "src", "content_scripts.ts"),
+  },
+  output: {
+    // distディレクトリにcontent_scripts.jsを吐く
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    // publicディレクトリにあるファイルをdistディレクトリにコピーする
+    new CopyWebpackPlugin({ patterns: [{ from: "public", to: "." }] }),
+  ],
+};
+
+export default config;
